47b6168f2a73f0ee4fdcc4b419882f40e9196cef
Heiko W. Rupp <hwr@pilhuhn.de>
2014-05-14 14:03:20 +0200

    Update the example to finally run and print messages when a packet is received.

--- netty-mc/src/main/java/de/bsd/nettymc/Main.java
+++ netty-mc/src/main/java/de/bsd/nettymc/Main.java
@@ -16,17 +16,23 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.DatagramPacket;
-import io.netty.channel.socket.oio.OioDatagramChannel;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 /**
- * Simple client (proxy) that receives messages via multicast
+ * Simple client that receives messages via multicast
+ * (in this case from Ganglia).
+ *
+ * For this to work, a gmond must be either running on the local
+ * machine or its sender ttl must be set high enough. The default
+ * of 1 will not work to receive data from a remote box.
+ *
  * @author Heiko W. Rupp
  */
 public class Main {
@@ -35,7 +41,6 @@ public class Main {
 
     private static final String GROUP = "239.2.11.71";
     private static final int PORT = 8649;
-    private static final String NETWORK_INTERFACE = "en5";
 
     public static void main(String[] args) throws Exception {
         Main main = new Main(args);
@@ -46,62 +51,31 @@ public class Main {
     }
 
     private void run() throws Exception {
-        EventLoopGroup oiogroup = new OioEventLoopGroup();
+        EventLoopGroup group = new NioEventLoopGroup();
         try {
 
+            // Obtain the interface for multicast reception
+            // Should be the one that has the ip address that is
+            // equivalent to the host name
+            Inet4Address hostAddr = (Inet4Address) InetAddress.getLocalHost();
+            NetworkInterface mcIf = NetworkInterface.getByInetAddress(hostAddr);
+
             // Try to set up an upd listener for Ganglia Messages
-            // TODO there is something fishy still wrt reception of packets
-
-/*
-            tcpdump sees packets coming in to the MC-group and also with the
-            right port:
-
-            sh-3.2# tcpdump -i en5 dst 239.2.11.71
-            tcpdump: verbose output suppressed, use -v or -vv for full protocol decode
-            listening on en5, link-type EN10MB (Ethernet), capture size 65535 bytes
-            09:06:12.262522 IP mackie.55850 > 239.2.11.71.8649: UDP, length 48
-            09:06:12.262907 IP mackie.55850 > 239.2.11.71.8649: UDP, length 44
-
-            Join seems to happen correctly:
-
-            $ netstat -gsv
-            en5:
-            	inet 172.31.7.7
-            	igmpv2 flags=0<>
-            		group 239.2.11.71 mode exclude
-            			mcast-macaddr 01:00:5e:02:0b:47
-
-            I also see the membership report on start and leave reports on program exit
-*/
-
-            InetAddress groupAddress = InetAddress.getByName(GROUP);
             InetSocketAddress gangliaSocket = new InetSocketAddress(GROUP, PORT);
-            InetSocketAddress localSocket = new InetSocketAddress("172.31.7.9",PORT);
-
-            NetworkInterface mcIf = NetworkInterface.getByName(NETWORK_INTERFACE);  // TODO determine from main address
 
             Bootstrap bootstrap = new Bootstrap();
-            InetSocketAddress remoteSocket = new InetSocketAddress("0.0.0.0",0);
             bootstrap
-                .group(oiogroup)
-                .channel(OioDatagramChannel.class)
-//                .option(ChannelOption.IP_MULTICAST_IF, mcIf) // Needed at all? - want to receive on all IF
+                .group(group)
+                .channel(NioDatagramChannel.class)
                 .option(ChannelOption.SO_REUSEADDR, true)
+                .option(ChannelOption.IP_MULTICAST_IF,mcIf)
 
-                .localAddress(gangliaSocket) // => Netty listening on udp /239.2.11.71:8649
-//              .localAddress(8649) // => Netty listening on udp 0.0.0.0/0.0.0.0:8649 OR BindException: Address already in use
-//                .localAddress(localSocket) // => Netty listening on udp /172.31.7.7:8649
-
-                .remoteAddress(remoteSocket) // Makes no difference
+                .localAddress(gangliaSocket)
                 .handler(new ChannelInitializer<Channel>() {
                     @Override
                     public void initChannel(Channel socketChannel) throws Exception {
                         ChannelPipeline pipeline = socketChannel.pipeline();
                         pipeline.addLast(new SimpleChannelInboundHandler<DatagramPacket>() {
-                            @Override
-                            public boolean acceptInboundMessage(Object msg) throws Exception {
-                                return true;
-                            }
 
                             @Override
                             protected void channelRead0(ChannelHandlerContext ctx,
@@ -113,26 +87,24 @@ public class Main {
                 })
             ;
 
-            logger.info("Bootstrap is " + bootstrap);
             ChannelFuture gangliaFuture = bootstrap.bind().sync();
             logger.info("Netty listening on udp " + gangliaFuture.channel().localAddress());
             DatagramChannel channel = (DatagramChannel) gangliaFuture.channel();
 
-            // Next two lines makes no apparent difference which one to use
-//            channel.joinGroup(gangliaSocket, mcIf).sync().addListener(new ChannelFutureListener() {
-            channel.joinGroup(groupAddress).sync().addListener(new ChannelFutureListener() {
+            channel.joinGroup(gangliaSocket, mcIf).sync().addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) throws Exception {
                     if (!future.isSuccess()) {
                         logger.error("Join failed: " + future.cause());
                     }
+                    else {
+                        logger.info("Joined the group");
+                    }
                 }
             });
-            logger.info("Joined the group");
             channel.closeFuture().sync();
-
         } finally {
-            oiogroup.shutdownGracefully().sync();
+            group.shutdownGracefully().sync();
         }
     }
 
