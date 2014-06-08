df2d36da496df23ecbfae21a962c35cd2d087ec3
Heiko W. Rupp <hwr@pilhuhn.de>
2014-06-08 21:26:32 +0200

    Test-app for some AsyncListener + Future issue.

---
+++ web-goo/src/main/java/de/bsd/webGoo/GooApp.java
@@ -0,0 +1,14 @@
+package de.bsd.webGoo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author Heiko W. Rupp
+ */
+@ApplicationPath("/")
+public class GooApp extends Application{
+
+    public GooApp() {
+    }
+}
